a46270b24b4718dd0cf77f3cc12dd4ffe79d9e23
David Klein <david.klein@psmtec.de>
2017-12-12 14:46:19 +0100

    type the options

--- graphql/Tools.hx
+++ graphql/Tools.hx
@@ -1,6 +1,12 @@
 package graphql;
 
+typedef Opts = {
+    typeDefs: Dynamic,
+    resolvers: Dynamic,
+    ?printErrors: Bool,
+}
+
 @:jsRequire('graphql-tools')
 extern class Tools {
-    public static function makeExecutableSchema( a: Dynamic ) : Dynamic;
+    public static function makeExecutableSchema( opts: Opts ) : Dynamic;
 }
